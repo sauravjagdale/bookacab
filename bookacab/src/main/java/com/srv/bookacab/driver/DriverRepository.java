08c282bf1a1e1f5f7488c1453cb3ab2d6de6b15f
Srv Jagdale <sauravjagdale4@github.com>
2019-09-29 23:58:52 +0530

    Update DriverRepository.java

--- bookacab/src/main/java/com/srv/bookacab/driver/DriverRepository.java
+++ bookacab/src/main/java/com/srv/bookacab/driver/DriverRepository.java
@@ -1,6 +1,6 @@
 package com.srv.bookacab.driver;
 
-import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DriverRepository extends JpaRepository<DriverEntity, Long> {
 	
-	@Query("SELECT d ,(3959 *acos(cos(radians(latitude)) * cos(radians(d.latitude)) * cos(radians(d.longitude) - radians(longitude)) + sin(radians(latitude)) * sin(radians(d.latitude)))) AS distance FROM DriverEntitiy d ORDER BY distance")
-	public List<DriverEntity> findNearestDriver(Double latitude, Double longitude);
+	@Query(value="SELECT (3959 *acos(cos(radians(:latitude)) * cos(radians(d.latitude)) * cos(radians(d.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(d.latitude)))) AS distance, d FROM DriverEntitiy d ORDER BY distance", nativeQuery = true)
+	public Map<String, DriverEntity> findNearestDriver(Double latitude, Double longitude);
 
 }
